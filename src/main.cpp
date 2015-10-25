99ef0a8de11d9af9d74f7930f83bd23365116899
MarcoFalke <falke.marco@gmail.com>
2015-10-25 03:01:20 +0100

    Clarify what minrelaytxfee does

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,7 @@ size_t nCoinCacheUsage = 5000 * 300;
 uint64_t nPruneTarget = 0;
 bool fAlerts = DEFAULT_ALERTS;
 
-/** Fees smaller than this (in satoshi) are considered zero fee (for relaying and mining) */
+/** Fees smaller than this (in satoshi) are considered zero fee (for relaying, mining and transaction creation) */
 CFeeRate minRelayTxFee = CFeeRate(DEFAULT_MIN_RELAY_TX_FEE);
 
 CTxMemPool mempool(::minRelayTxFee);
