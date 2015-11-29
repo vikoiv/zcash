3c5204d0487d8e4d290ed1f73d5640f27bf884d7
Patick Strateman <patrick.strateman@gmail.com>
2015-11-29 01:52:51 -0800

    Rename setInventoryKnown filterInventoryKnown

--- src/net.h
+++ src/net.h
@@ -375,7 +375,7 @@ public:
     std::set<uint256> setKnown;
 
     // inventory based relay
-    CRollingBloomFilter setInventoryKnown;
+    CRollingBloomFilter filterInventoryKnown;
     std::vector<CInv> vInventoryToSend;
     CCriticalSection cs_inventory;
     std::set<uint256> setAskFor;
@@ -484,7 +484,7 @@ public:
     {
         {
             LOCK(cs_inventory);
-            setInventoryKnown.insert(inv.hash);
+            filterInventoryKnown.insert(inv.hash);
         }
     }
 
@@ -492,7 +492,7 @@ public:
     {
         {
             LOCK(cs_inventory);
-            if (!setInventoryKnown.contains(inv.hash))
+            if (!filterInventoryKnown.contains(inv.hash))
                 vInventoryToSend.push_back(inv);
         }
     }
