73187b2e63c09570ae3c1fd3183ee98fd7ca64b7
Braydon Fuller <courier@braydon.com>
2016-03-17 16:06:08 -0400

    rpc: added getaddressbalance method using addressindex

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -155,6 +155,7 @@ extern void EnsureWalletIsUnlocked();
 
 extern json_spirit::Value getconnectioncount(const json_spirit::Array& params, bool fHelp); // in rpcnet.cpp
 extern json_spirit::Value getaddresstxids(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getaddressbalance(const json_spirit::Array& params, bool fHelp);
 
 extern json_spirit::Value getpeerinfo(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value ping(const json_spirit::Array& params, bool fHelp);
