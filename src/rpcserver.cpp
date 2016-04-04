7a1d3838c8fe571fbf30d667e0199a7439063acc
Braydon Fuller <courier@braydon.com>
2016-04-04 16:37:43 -0400

    main: mempool address index

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -328,6 +328,7 @@ static const CRPCCommand vRPCCommands[] =
     { "rawtransactions",    "signrawtransaction",     &signrawtransaction,     false }, /* uses wallet if enabled */
 
     /* Address index */
+    { "addressindex",       "getaddressmempool",      &getaddressmempool,      false },
     { "addressindex",       "getaddressutxos",        &getaddressutxos,        false },
     { "addressindex",       "getaddressdeltas",       &getaddressdeltas,       false },
     { "addressindex",       "getaddresstxids",        &getaddresstxids,        false },
