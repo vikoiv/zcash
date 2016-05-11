c7bb82e83c63cfc150328808e1dba484aada5886
Braydon Fuller <braydon@bitpay.com>
2016-05-11 17:17:08 -0400

    rpcclient: add params to be parsed as JSON
    There was an issue where getblockhashes wouldn't work from bitcoin-cli
    as the two params would be strings instead of integers. This fixes that
    issue, and will parse the first param as JSON for other addressindex
    related rpc methods.

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -105,7 +105,15 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "z_sendmany", 2},
     { "z_getoperationstatus", 0},
     { "z_getoperationresult", 0},
-    { "z_importkey", 1 }
+    { "z_importkey", 1 },
+    { "getblockhashes", 0 },
+    { "getblockhashes", 1 },
+    { "getspentinfo", 0},
+    { "getaddresstxids", 0},
+    { "getaddressbalance", 0},
+    { "getaddressdeltas", 0},
+    { "getaddressutxos", 0},
+    { "getaddressmempool", 0},
 };
 
 class CRPCConvertTable
