1349d53a17a72921923d16971e5d30d6580a8f9d
Braydon Fuller <braydon@bitpay.com>
2016-05-16 14:23:01 -0400

    main: spentindex for the mempool
    Zcash: Adapted to older mempool code

--- src/main.cpp
+++ src/main.cpp
@@ -1259,8 +1259,15 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
 
         // Store transaction in memory
         pool.addUnchecked(hash, entry, !IsInitialBlockDownload());
+
+        // Add memory address index
         if (fAddressIndex) {
             pool.addAddressIndex(entry, view);
+        }
+
+        // Add memory spent index
+        if (fSpentIndex) {
+            pool.addSpentIndex(entry, view);
         }
     }
 
@@ -1284,6 +1291,9 @@ bool GetSpentIndex(CSpentIndexKey &key, CSpentIndexValue &value)
 {
     if (!fSpentIndex)
         return false;
+
+    if (mempool.getSpentIndex(key, value))
+        return true;
 
     if (!pblocktree->ReadSpentIndex(key, value))
         return error("unable to get spent info");
