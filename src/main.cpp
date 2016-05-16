009bbd7ee9d62eabf8791f24178700d8231d9c28
Braydon Fuller <braydon@bitpay.com>
2016-05-16 14:23:01 -0400

    main: spentindex for the mempool

--- src/main.cpp
+++ src/main.cpp
@@ -1243,8 +1243,15 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
 
         // Store transaction in memory
         pool.addUnchecked(hash, entry, !IsInitialBlockDownload());
+
+        // Add memory address index
         if (fAddressIndex) {
             pool.addAddressIndex(entry, view);
+        }
+
+        // Add memory spent index
+        if (fSpentIndex) {
+            pool.addSpentIndex(entry, view);
         }
     }
 
@@ -1268,6 +1275,9 @@ bool GetSpentIndex(CSpentIndexKey &key, CSpentIndexValue &value)
 {
     if (!fSpentIndex)
         return false;
+
+    if (mempool.getSpentIndex(key, value))
+        return true;
 
     if (!pblocktree->ReadSpentIndex(key, value))
         return error("unable to get spent info");
