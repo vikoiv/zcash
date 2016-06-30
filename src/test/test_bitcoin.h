dcee290595e1ec58edfb67667f0f2313cf1fcfb0
Chethan Krishna <chethan@bitpay.com>
2016-06-30 18:00:50 -0400

    wallet-utility: extract addresses and private keys
    usage: ./wallet-utility -datadir=<directory>
    help: ./wallet-utility -h
    
    Zcash: Backported to older chainparams code, added Zcash libs to LDADD

--- src/test/test_bitcoin.h
+++ src/test/test_bitcoin.h
@@ -1,6 +1,7 @@
 #ifndef BITCOIN_TEST_TEST_BITCOIN_H
 #define BITCOIN_TEST_TEST_BITCOIN_H
 
+#include "chainparamsbase.h"
 #include "txdb.h"
 
 #include <boost/filesystem.hpp>
@@ -27,4 +28,14 @@ struct TestingSetup: public BasicTestingSetup {
     ~TestingSetup();
 };
 
+/** Wallet setup that configures a complete environment.
+ * Included are data directory, coins database, script check threads
+ * and wallet with 5 unused keys.
+ */
+struct WalletSetup: public BasicTestingSetup {
+	boost::filesystem::path pathTemp;
+
+	WalletSetup(CBaseChainParams::Network network = CBaseChainParams::MAIN);
+	~WalletSetup();
+};
 #endif
