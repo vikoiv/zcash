605a5010aac405ac01b1d5e4fe839c9204bd64a9
Jack Grigg <jack@z.cash>
2018-01-05 15:52:05 +0000

    Update Rust dependency

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,10 +1,10 @@
 package=rust
-$(package)_version=1.16.0
+$(package)_version=1.23.0
 $(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name_linux=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash_linux=48621912c242753ba37cad5145df375eeba41c81079df46f93ffb4896542e8fd
+$(package)_sha256_hash_linux=9a34b23a82d7f3c91637e10ceefb424539dcfa327c2dcd292ff10c047b1fdc7e
 $(package)_file_name_darwin=rust-$($(package)_version)-x86_64-apple-darwin.tar.gz
-$(package)_sha256_hash_darwin=2d08259ee038d3a2c77a93f1a31fc59e7a1d6d1bbfcba3dba3c8213b2e5d1926
+$(package)_sha256_hash_darwin=9274e977322bb4b153f092255ac9bd85041142c73eaabf900cb2ef3d3abb2eba
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
