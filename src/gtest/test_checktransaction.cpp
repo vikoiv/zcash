9e96d8fc887c3c66f7443c9bdd55821322cfdf49
Jack Grigg <jack@z.cash>
2018-02-02 01:49:42 +0000

    Add consensus branch ID parameter to SignatureHash, remove SigVersion parameter
    We do not need to be able to calculate multiple SignatureHash versions for a
    single transaction format; instead, we use the transaction format to determine
    the SigVersion.
    
    The consensus branch ID *does* need to be passed in from the outside, as only
    the caller knows the context in which the SignatureHash is being calculated
    (ie. mempool acceptance vs. block validation).
    
    JoinSplit signature verification has been moved into ContextualCheckTransaction,
    where the consensus branch ID can be obtained.
    
    The argument to the sign command for zcash-tx has been modified to take a height
    in addition to the optional sigtype flags.

--- src/gtest/test_checktransaction.cpp
+++ src/gtest/test_checktransaction.cpp
@@ -45,6 +45,8 @@ public:
 
 
 CMutableTransaction GetValidTransaction() {
+    uint32_t consensusBranchId = SPROUT_BRANCH_ID;
+
     CMutableTransaction mtx;
     mtx.vin.resize(2);
     mtx.vin[0].prevout.hash = uint256S("0000000000000000000000000000000000000000000000000000000000000001");
@@ -74,7 +76,7 @@ CMutableTransaction GetValidTransaction() {
     // Empty output script.
     CScript scriptCode;
     CTransaction signTx(mtx);
-    uint256 dataToBeSigned = SignatureHash(scriptCode, signTx, NOT_AN_INPUT, SIGHASH_ALL, 0, SIGVERSION_BASE);
+    uint256 dataToBeSigned = SignatureHash(scriptCode, signTx, NOT_AN_INPUT, SIGHASH_ALL, 0, consensusBranchId);
     if (dataToBeSigned == one) {
         throw std::runtime_error("SignatureHash failed");
     }
