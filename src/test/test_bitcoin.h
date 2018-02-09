198296ee20b9e764eaa9ae07d529e44977bdada5
Jack Grigg <jack@z.cash>
2018-02-09 23:16:55 +0000

    Remove mempool transactions which commit to an unmineable branch ID

--- src/test/test_bitcoin.h
+++ src/test/test_bitcoin.h
@@ -48,10 +48,12 @@ struct TestMemPoolEntryHelper
     unsigned int nHeight;
     bool hadNoDependencies;
     bool spendsCoinbase;
+    uint32_t nBranchId;
 
     TestMemPoolEntryHelper() :
         nFee(0), nTime(0), dPriority(0.0), nHeight(1),
-        hadNoDependencies(false), spendsCoinbase(false) { }
+        hadNoDependencies(false), spendsCoinbase(false),
+        nBranchId(0) { } // TODO: SPROUT_BRANCH_ID
 
     CTxMemPoolEntry FromTx(CMutableTransaction &tx, CTxMemPool *pool = NULL);
 
@@ -62,5 +64,6 @@ struct TestMemPoolEntryHelper
     TestMemPoolEntryHelper &Height(unsigned int _height) { nHeight = _height; return *this; }
     TestMemPoolEntryHelper &HadNoDependencies(bool _hnd) { hadNoDependencies = _hnd; return *this; }
     TestMemPoolEntryHelper &SpendsCoinbase(bool _flag) { spendsCoinbase = _flag; return *this; }
+    TestMemPoolEntryHelper &BranchId(uint32_t _branchId) { nBranchId = _branchId; return *this; }
 };
 #endif
